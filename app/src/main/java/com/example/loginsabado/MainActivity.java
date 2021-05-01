fdae81b040dd1af713f5e5b25e020c9dd4e5239c
estebanzeb <estebanzeb@gmail.com>
2021-05-01 13:11:58 -0500

    Nuevasimagenes

--- app/src/main/java/com/example/loginsabado/MainActivity.java
+++ app/src/main/java/com/example/loginsabado/MainActivity.java
@@ -12,8 +12,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     //ATRIBUTOS
-    EditText cajaHorasTrabajadas;
-    Button botonCalcularSalario;
     TextView cajaResultado,nombre,descripcion;
     ImageView foto, foto2,fondo;
 
@@ -27,15 +25,15 @@ public class MainActivity extends AppCompatActivity {
         nombre=findViewById(R.id.nombreMain);
         descripcion=findViewById(R.id.descripcionMain);
         foto=findViewById(R.id.fotoMain);
-        //foto2=findViewById(R.id.foto2);
-        //fondo=findViewById(R.id.fondo);
+        foto2=findViewById(R.id.foto2);
+        fondo=findViewById(R.id.fondo);
 
         empleado = (Trabajador)getIntent().getSerializableExtra("trabajador");
 
         nombre.setText(empleado.getNombre());
         descripcion.setText(empleado.getDescripcion());
         foto.setImageResource(empleado.getFoto());
-        //foto2.setImageResource(empleado.getFoto2());
-        //fondo.setImageResource(empleado.getFondo());
+        foto2.setImageResource(empleado.getFoto2());
+        fondo.setImageResource(empleado.getFondo());
     }
 }
